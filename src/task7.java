d46ad95d224414141ead133b5abe5265b7ae5f0f
Abylay <abylay0505@gmail.com>
2023-04-06 20:51:57 +0600

    comments added

--- src/task7.java
+++ src/task7.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class task7 {
     public static String findReverse(int n, Scanner sc){
-        int a;
-        if(n <= 1){
+        int a; // @ a = numbers that we want to reverse
+        if(n <= 1){ // base case
             a = sc.nextInt();
-            return String.valueOf(a);
+            return String.valueOf(a); // returning String, because of adding elements
         }
         a = sc.nextInt();
-        return findReverse(n - 1, sc) + " " + a;
+        return findReverse(n - 1, sc) + " " + a; // returning reverse of numbers by adding first numbers to the end
     }
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // @ n = amount of number
 
         System.out.println(findReverse(n, sc));
 
