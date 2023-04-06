c01f555dc6e07e61e3d7ad27d0442d1f2d5484ae
Abylay <abylay0505@gmail.com>
2023-04-06 20:21:35 +0600

    comments added

--- src/task3.java
+++ src/task3.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class task3 {
     public static String findPrime(int n) {
-        for(int i = 2; i<n/2; i++) {
-            if(n%i == 0) return "Composite";
+        for(int i = 2; i<n/2; i++) { // checking for prime by dividing numbers two times lesser than n
+            if(n%i == 0) return "Composite"; // if n is divisible by any number in this loop
         }
-        return "Prime";
+        return "Prime"; // else returning "Prime"
     }
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, input the number that you want to check: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // @ n = input number
         System.out.println("This number is " + findPrime(n));
 
         sc.close();
