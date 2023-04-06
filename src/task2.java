51cfb75a59b4371d75ea35bcacecef2e4a71c4cf
Abylay <abylay0505@gmail.com>
2023-04-06 20:11:32 +0600

    comments added

--- src/task2.java
+++ src/task2.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class task2 {
     public static double findAvg(int n, int[] arr) {
-        double sum = 0;
+        double sum = 0; // @ sum = sum of all elements
         for(int i = 0; i<n; i++) {
-            sum += arr[i];
+            sum += arr[i]; // adding all elements to get result
         }
-        return sum/n;
+        return sum/n; // returning the average number dy dividing to length
     }
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, write the length of your array: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // @ n = length of the array
         System.out.println("Please, input the numbers of your array:");
-        int[] arr = new int[n];
+        int[] arr = new int[n]; // @ arr = array
         for(int i = 0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
