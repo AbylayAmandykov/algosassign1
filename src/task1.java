d146e03323c33ff29450187b1fd7297f2cf02c46
Abylay <abylay0505@gmail.com>
2023-04-06 20:04:21 +0600

    eleventh commit

--- src/task1.java
+++ src/task1.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class task1 {
     public static int findMin(int n, int[] arr) {
-        int min = 10000000;
+        int min = 10000000; // @ min = our first minimum value
         for(int i = 0; i<n; i++) {
-            if(min > arr[i]) min = arr[i];
+            if(min > arr[i]) min = arr[i]; // finding the minimum by comparing each value
         }
-        return min;
+        return min; // returning the minimum value
     }
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, write the length of your array: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // @ n length of array
         System.out.println("Please, input the numbers of your array:");
-        int[] arr = new int[n];
+        int[] arr = new int[n]; // @ arr = array
         for(int i = 0; i<n; i++){
             arr[i] = sc.nextInt();
         }
