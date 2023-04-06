fb38e19e7bd0f07342030154a6d500e3dc2ca83a
Abylay <abylay0505@gmail.com>
2023-04-06 20:24:50 +0600

    comments added

--- src/task4.java
+++ src/task4.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class task4 {
     public static int findFactorial(int n) {
-        if(n==1) return 1;
-        return findFactorial(n-1) * n;
+        if(n==1) return 1; // base case
+        return findFactorial(n-1) * n; // returning factorized number by multiplying previous numbers
     }
 
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, enter your number: ");
-        int n = sc.nextInt();
-        int res = findFactorial(n);
+        int n = sc.nextInt(); // @ n = input value
+        int res = findFactorial(n); // getting our function
         System.out.println("Factorial of your number is: " + res);
 
         sc.close();
