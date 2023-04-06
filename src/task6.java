bd31f0261e4bb19e9a6a06877e4277cd88639635
Abylay <abylay0505@gmail.com>
2023-04-06 20:29:49 +0600

    comments added

--- src/task6.java
+++ src/task6.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class task6 {
     public static int findPower(int n, int m) {
-        if(m==0) return 1;
-        return n * findPower(n, m-1);
+        if(m==0) return 1; // base case
+        return n * findPower(n, m-1); // returning the power of n by multiplying each value
     }
 
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, enter your number: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // @ n = entered value
         System.out.println("Please, enter power of your number: ");
-        int m = sc.nextInt();
-        int res = findPower(n, m);
+        int m = sc.nextInt(); // @ m = power of n
+        int res = findPower(n, m); // getting our function
         System.out.println(m + " to the power of " + n + " is: "+ res);
 
         sc.close();
