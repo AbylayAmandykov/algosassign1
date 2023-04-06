b9877d7ff72807248147b0b9e09a9a87c70fb1ad
Abylay <abylay0505@gmail.com>
2023-04-06 20:43:29 +0600

    comments added

--- src/task8.java
+++ src/task8.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class task8 {
     public static String findDigit(String s, int i) {
         if(i < 0){
-            return "YES";
+            return "YES"; // base case
         }
-        if(s.charAt(i) >=48 && s.charAt(i)<= 58){
-            return findDigit(s, i - 1);
+        if(s.charAt(i) >=48 && s.charAt(i)<= 58){ // checking each value of s by ASCII table, there are only digits between 48 and 58
+            return findDigit(s, i - 1); // returning "Yes" if the first condition passed
         }
-        return "NO";
+        return "NO"; // else returning "NO"
     }
 
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String ans = findDigit(s, s.length() - 1);
+        String s = sc.nextLine(); // @ s = string that we want to check
+        String ans = findDigit(s, s.length() - 1); // getting our function
         System.out.println(ans);
 
         sc.close();
