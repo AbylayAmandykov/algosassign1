46a4414b3a4888c6f28267875650e54876728db8
Abylay <abylay0505@gmail.com>
2023-04-06 20:36:47 +0600

    comments added

--- src/task10.java
+++ src/task10.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class task10 {
     public static int findGcd(int a, int b) {
-        if(b==0) return a;
-        return findGcd(b, a%b);
+        if(b==0) return a; // base case
+        return findGcd(b, a%b); // returning the greatest common divisor by taking mod of two elements
 
     }
 
     public static void main(String[] args) {
+        // getting input
         Scanner sc = new Scanner(System.in);
         System.out.println("Please, enter the first number: ");
-        int a = sc.nextInt();
+        int a = sc.nextInt(); // @ a = first entered value
         System.out.println("Please, enter the second number: ");
-        int b = sc.nextInt();
-        int res = findGcd(a, b);
+        int b = sc.nextInt(); // @ b = second entered value
+        int res = findGcd(a, b); // getting our function
         System.out.println("The GCD of numbers " + a + " and " + b + " is: " + res);
 
         sc.close();
